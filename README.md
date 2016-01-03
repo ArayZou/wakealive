74f5a8ddc4cfc9798a33b4ce7a0f1282a7f94b63
arayzou <rui.zou@dianping.com>
2016-01-03 10:27:27 +0800

    generator 0.0.2

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # wakealive
 
+# 设计思路
+
+前端系统和后端系统分离
+
+theme负责最后打包生成的博客页面
+theme全静态redux-SPA，通过webpack跑本地调试server和build发布包
+
+server负责本地后台编辑设置页面，最后生成json文件夹放入build
+server用KOA加后端渲染react的模式
+
+最后上传build里的文件或者直接webpack起build里的静态目录server
+
+# 文件目录
+
+├── /theme/
+    └──  /wakealive
+├── /build/
+│   ├── /index.html
+│   ├── /bundel.js
+│   └──  /json/
+├── /server/
+│   ├── /server.js
+│   └──  /tpl
+│── package.json
+└── webpack.config.js
+
+# Learn More
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,45 @@
-/* eslint no-var: 0 */
-require("./register-babel");
-var config = require("./webpack/webpack.config");
-var result = config();
-module.exports = result;
+var rucksack = require('rucksack-css')
+
+module.exports = {
+  context: __dirname + "/theme",
+  entry: {
+    js: "./wakealive/index.js",
+    html: "./wakealive/index.html",
+  },
+  output: {
+    path: __dirname + "/build",
+    filename: "bundle.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]"
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          "style-loader",
+          "css-loader?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]",
+          "postcss-loader"
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loaders: [
+          "react-hot",
+          "babel-loader"
+        ]
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  postcss: [
+    rucksack({
+      autoprefixer: true
+    })
+  ]
+}
